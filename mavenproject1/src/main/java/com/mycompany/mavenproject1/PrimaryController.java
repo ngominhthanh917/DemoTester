a577e3decfb67959394ddde961b337a75fc03fbe
2051052127thanh@ou.edu.vn <2051052127thanh@ou.edu.vn>
2023-02-17 16:53:34 +0700

    Resolve

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/PrimaryController.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/PrimaryController.java
@@ -7,8 +7,5 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        int x = 20;
-        if(x == 20 && x%2 == 0)
-            App.setRoot("secondary");
     }
 }
